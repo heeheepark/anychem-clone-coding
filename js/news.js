2438b6e09fc18ead7753de5e529ffb1bd83daf3c
heeheepark <pjh9641@gmail.com>
2023-06-09 18:03:31 +0900

    feat: news 영역 json 연동 및 반응형 완료, info 영역 진행중

--- js/news.js
+++ js/news.js
@@ -1,10 +1,9 @@
 window.addEventListener('load', function () {
   // 외부 데이터 로딩
-  // this.fetch('data/newsdata.json')
-  //   .then((res) => res.json())
-  //   .then((result) => console.log(result))
-  //   .catch((err) => console.log(err));
-
+  //   fetch("data/newsdata.json")
+  //     .then((res) => res.json())
+  //     .then((result) => console.log(result))
+  //     .catch((err) => console.log(err));
   const getNews = async () => {
     try {
       const res = await fetch('data/newsdata.json');
@@ -14,27 +13,55 @@ window.addEventListener('load', function () {
       console.log(err);
     }
   };
+
+  let makeHtml = (_data) => {
+    let html = ``;
+    _data.forEach((item) => {
+      let tag = `
+        <div class="swiper-slide">
+            <div class="news-box">
+            <a href="${item.link}">
+                <span class="news-cate">${item.cate}</span>
+                <p>${item.title}</p>
+                <span class="news-date">
+                    <i class="fa-regular fa-clock"></i>
+                    ${item.date}
+                </span>
+            </a>
+            </div>
+        </div>
+        `;
+
+      html += tag;
+    });
+    const newsWrap = document.querySelector('.sw-news .swiper-wrapper');
+    console.log(html);
+    newsWrap.innerHTML = html;
+
+    // swiper 생성
+    new Swiper('.sw-news', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+      },
+      pagination: {
+        el: '.sw-news-pg',
+        clickable: true,
+      },
+      speed: 800,
+      autoplay: {
+        delay: 2500,
+      },
+    });
+  };
+
   getNews();
 });
-
-let makeHtml = (_data) => {
-  let html = ``;
-  _data.forEach((item) => {
-    let tag = `
-    <div class="swiper-slide">
-      <div class="news-box">
-        <a href="${item.link}">
-          <span>${item.cate}</span>
-          <p>${item.title}</p>
-          <span>
-            <i class="fa-regular fa-clock"></i>
-            ${item.date}</span
-          >
-        </a>
-      </div>
-    </div>
-    `;
-    html += tag;
-    console.log(html);
-  });
-};
