307350bb293865a0918a3f9765e2bb913e2af64b
heeheepark <pjh9641@gmail.com>
2023-06-17 19:56:46 +0900

    클론코드 안내창(모달창) 추가

---
+++ js/index.js
@@ -0,0 +1,24 @@
+window.onload = function () {
+  // 모달창 처리
+  let body = document.querySelector("body");
+  let modal = document.querySelector(".modal");
+  const modalClose = document.querySelector(".modal-cont .closeBtn");
+  console.log(modalClose);
+  body.classList.add("modal-active");
+  modalClose.onclick = function () {
+    body.classList.remove("modal-active");
+    modal.style.display = "none";
+  };
+
+  // 모달창 아이콘
+  const icon = document.querySelectorAll(".icon");
+  const iconInfo = document.querySelectorAll(".icon-info");
+  icon.forEach((item, index) => {
+    item.addEventListener("mouseenter", function () {
+      iconInfo[index].style.display = "block";
+    });
+    item.addEventListener("mouseleave", function () {
+      iconInfo[index].style.display = "none";
+    });
+  });
+};
