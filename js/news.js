07f8353a8b1d4f853b060dbe579c517382305ea5
heeheepark <pjh9641@gmail.com>
2023-06-17 19:08:28 +0900

    news 영역 swiper 수정 및 json data 수정

--- js/news.js
+++ js/news.js
@@ -1,4 +1,4 @@
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
   // 외부 데이터 로딩
   //   fetch("data/newsdata.json")
   //     .then((res) => res.json())
@@ -6,7 +6,7 @@ window.addEventListener('load', function () {
   //     .catch((err) => console.log(err));
   const getNews = async () => {
     try {
-      const res = await fetch('data/newsdata.json');
+      const res = await fetch("data/newsdata.json");
       const result = await res.json();
       makeHtml(result);
     } catch (err) {
@@ -34,31 +34,32 @@ window.addEventListener('load', function () {
 
       html += tag;
     });
-    const newsWrap = document.querySelector('.sw-news .swiper-wrapper');
+    const newsWrap = document.querySelector(".sw-news .swiper-wrapper");
     console.log(html);
     newsWrap.innerHTML = html;
 
     // swiper 생성
-    new Swiper('.sw-news', {
+    const newsSwiper = new Swiper(".sw-news", {
+      loop: true,
       slidesPerView: 4,
       spaceBetween: 20,
-      breakpoints: {
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-      },
       pagination: {
-        el: '.sw-news-pg',
+        el: ".sw-news-pg",
         clickable: true,
       },
       speed: 800,
       autoplay: {
         delay: 2500,
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: 3, //브라우저가 1024보다 클 때
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2, //브라우저가 768보다 클 때
+          spaceBetween: 10,
+        },
       },
     });
   };
